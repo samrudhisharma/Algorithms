ab47135215e87a9524cb046d2ee3e0308bfd74d9
Samrudhi Sharma <samrudhi.s21@gmail.com>
2015-11-05 12:27:06 -0500

    print LL

---
+++ PIQ/PrintElementsOFLLUsingRecursion.java
@@ -0,0 +1,23 @@
+/*class Node
+ * {
+ * int data;
+ * Node next;
+ * }
+ * 
+ * void Print(Node head)
+ * {
+ * 
+ * 	if(head==null)
+ * 		return;
+ *  // recursion - forward print
+ *	System.out.println(head.data);
+ *	Print(head.next);
+ * }
+ * 
+ * void ReversePrint(Node head)
+ * {
+ *  if(head==null){return;}
+ * 	ReversePrint(head.next);
+ * 	System.out.println(head.data);
+ * }
+ * */
