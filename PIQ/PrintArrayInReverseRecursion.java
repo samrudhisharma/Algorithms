c47b35191384cd26eb4942ce171adb2d0ff322e6
Samrudhi Sharma <samrudhi.s21@gmail.com>
2015-11-05 10:30:21 -0500

    LL 2

---
+++ PIQ/PrintArrayInReverseRecursion.java
@@ -0,0 +1,17 @@
+/*class Node
+ * {
+ * int data;
+ * Node next;
+ * }
+ * 
+ * void ReversePrint(Node root)
+ * {
+ * 
+ * if(root==null) return;
+ * ReversePrint(root.next);
+ * System.out.println(root.data);
+ * 
+ * }
+ *  
+ * */
+ 
