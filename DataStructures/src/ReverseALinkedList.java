4aad6aeb3ed4b13b280bd7039e7b892f8ae9b16b
Samrudhi Sharma <samrudhi.s21@gmail.com>
2015-11-15 18:29:56 -0500

    revLL

--- DataStructures/src/ReverseALinkedList.java
+++ DataStructures/src/ReverseALinkedList.java
@@ -4,27 +4,43 @@ public class ReverseALinkedList {
 	public static void main(String[] args) {
 	
 		int arr[] = {1,2,3,4,5,6};
-		LinkedList ll = new LinkedList();
-		
-		
+		LinkedList head = new LinkedList();
+		head.data = arr[0];
+		LinkedList temp;
+		temp = head;
 		for(int i=0;i<arr.length;i++)
 		{
-			ll.data = arr[i];
-			ll = ll.next;
+			temp.next = new LinkedList();
+			temp.data = arr[i];
+			temp = temp.next;
 		}
 		
-		printLinkedList(ll);
+		printLinkedList(reverseLinkedList(head));
 
 	}
 	
-	static void reverseLinkedList()
+	static LinkedList reverseLinkedList(LinkedList ll)
 	{
+		LinkedList prev = null;
+		LinkedList temp;
+		while(ll!=null)
+		{
+			temp = ll.next;
+			ll.next = prev;
+			prev = ll;
+			ll = temp;
+		}
 		
+		return prev;
 	}
 	
 	static void printLinkedList(LinkedList ll)
 	{
-	
+		while(ll!=null)
+		{
+			System.out.println(ll.data);
+			ll = ll.next;
+		}
 	}
 
 }
